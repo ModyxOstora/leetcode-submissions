cef1539799ba0c46b5de0fa199d91bfea67b2f3f
ModyxOstora <moayad20072017@gmail.com>
2026-06-06 02:03:50 +0300

    Time: 7 ms (54.22%) | Memory: 13.2 MB (60.47%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
+    inline bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
     int maxVowels(string s, int k) {
-        unordered_set<char> vowels = { 'a','e','i','o','u' };
-        int left = 1;
-        int right = k;
-        int mx = 0;
-        for (int i = 0;i < k;i++) {
-            if (vowels.count(s[i])) mx++;
+        int vowels_count = 0;
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s[i])) vowels_count++;
         }
-        int vowels_count = mx;
-        while (right < s.length()) {
-            if (vowels.count(s[left - 1])) vowels_count--;
-            if (vowels.count(s[right])) vowels_count++;
-            if (mx < vowels_count) mx = vowels_count;
-            left++;
-            right++;
+        int mx = vowels_count;
+        for (int i = k; i < s.length(); i++) {
+            if (isVowel(s[i])) vowels_count++;
+            if (isVowel(s[i - k])) vowels_count--;
+            if (vowels_count > mx) mx = vowels_count;
         }
-
+        
         return mx;
     }
 };
